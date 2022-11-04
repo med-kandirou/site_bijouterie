df106e196db0da8d629689937a2f7fa07f1f7e82
kandirou0 <simo.kandirou15@gmail.com>
2022-11-04 15:04:34 +0100

    update

--- contact.php
+++ contact.php
@@ -1,13 +1,72 @@
-<?php
-  require_once 'includes/user/header.php';
-?>
+<!DOCTYPE html>
+<html>
+
+<head>
+  <!-- Basic -->
+  <meta charset="utf-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <!-- Mobile Metas -->
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+  <!-- Site Metas -->
+  <meta name="keywords" content="les bijoux en or ,gifts, cadeau, dating , love, family, marriage,commitment"/>
+  <meta name="description" content="ce site web est boutique des modernes bijoux avec bon qualité et meilleur prix avec garantie ..." />
+  <meta name="author" content="mohamed kandirou"/>
+
+  <title>Lodge</title>
+
+  <!-- slider stylesheet -->
+  <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.1.3/assets/owl.carousel.min.css" />
+
+  <!-- bootstrap core css -->
+  <link rel="stylesheet" type="text/css" href="assets/css/bootstrap.css" />
+
+  <!-- fonts style -->
+  <link href="https://fonts.googleapis.com/css?family=Baloo+Chettan|Poppins:400,600,700&display=swap" rel="stylesheet">
+  <!-- Custom styles for this template -->
+  <link href="assets/css/style.css" rel="stylesheet"/>
+  <link href="assets/css/MonStyle.css" rel="stylesheet"/>
+  <!-- responsive style -->
+  <link href="assets/css/responsive.css" rel="stylesheet"/>
+</head>
 <body class="sub_page">
 
   <div class="hero_area">
     <!-- header section strats -->
-  <?php
-    require_once 'includes/user/navbar.php';
-  ?>
+    <header class="header_section">
+      <div class="container-fluid">
+        <nav class="navbar navbar-expand-lg custom_nav-container ">
+          <a class="navbar-brand" href="index.html">
+            <img src="./assets/images/logo.png" alt="logo de site">
+          </a>
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <div class="d-flex ml-auto flex-column flex-lg-row align-items-center">
+              <ul class="navbar-nav  ">
+                <li class="nav-item active">
+                  <a class="nav-link" href="./index.php">Home <span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="./about.php"> About</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="./gallery.php"> Gallery</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="./contact.php"> Contact</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="./connect.php">Connecter</a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </nav>
+      </div>
+    </header>
+    <!-- end header section -->
     <!-- end header section -->
   </div>
 
