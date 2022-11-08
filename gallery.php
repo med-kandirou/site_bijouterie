65e426eb3823ee84ef0db208d34c0f028c9c6079
kandirou0 <simo.kandirou15@gmail.com>
2022-11-08 14:45:55 +0100

    update

--- gallery.php
+++ gallery.php
@@ -1,3 +1,6 @@
+<?php
+  require_once 'includes/classes/config.php';
+ ?>
 <!DOCTYPE html>
 <html>
 
--- index.php
+++ index.php
@@ -1,4 +1,6 @@
-
+<?php
+  require_once 'includes/classes/config.php';
+ ?>
 <!DOCTYPE html>
 <html>
 
@@ -194,55 +196,31 @@
   </div>
 
   <!-- item section -->
-
   <div class="item_section layout_padding2">
     <div class="container">
       <div class="item_container">
-        <div class="box">
+
+      <?php
+        $db=new database();
+        $query="select * from produits";
+        $stmt=$db->openConnection()->query("SELECT `id_produit`, `nom_prod`, `image` FROM `produit` ORDER BY prix LIMIT 3");
+        while ($row = $stmt->fetch()){
+          echo '
+          <div class="box">
           <div class="price">
             <h6>
-              Best PRICE
+              Meilleur prix
             </h6>
           </div>
           <div class="img-box">
-            <img src="assets/images/i-1.png" alt="bijoux d'or">
+            <img src="assets/images/'.$row['image'].'" alt="bijoux">
           </div>
           <div class="name">
-            <h5>
-              Bracelet
-            </h5>
+            <h5>'.$row['nom_prod'].'</h5>
           </div>
-        </div>
-        <div class="box">
-          <div class="price">
-            <h6>
-              Best PRICE
-            </h6>
-          </div>
-          <div class="img-box">
-            <img src="assets/images/i-2.png" alt="bijoux d'or 2">
-          </div>
-          <div class="name">
-            <h5>
-              Ring
-            </h5>
-          </div>
-        </div>
-        <div class="box">
-          <div class="price">
-            <h6>
-              Best PRICE
-            </h6>
-          </div>
-          <div class="img-box">
-            <img src="assets/images/i-3.png" alt="bijoux d'or 3">
-          </div>
-          <div class="name">
-            <h5>
-              Earings
-            </h5>
-          </div>
-        </div>
+         </div> ';
+      }
+      ?>
       </div>
     </div>
   </div>
@@ -298,63 +276,36 @@
         </h2>
       </div>
       <div class="price_container">
-        <div class="box">
+
+      <?php
+        $db=new database();
+        $query="select * from produits";
+        $stmt=$db->openConnection()->query("SELECT `id_produit`, `nom_prod`,`prix`, `image` FROM `produit` ORDER BY prix desc LIMIT 3");
+        while ($row = $stmt->fetch()){
+          echo '
+          <div class="box">
           <div class="name">
             <h6>
-              Diamond Ring
+              '.$row['nom_prod'].'
             </h6>
           </div>
           <div class="img-box">
-            <img src="assets/images/p-1.png" alt="prod 1">
+            <img src="assets/images/'.$row['image'].'" alt="prod '.$row['id_produit'].'">
           </div>
           <div class="detail-box">
             <h5>
-              $<span>1000.00</span>
+              $<span>'.$row['prix'].'</span>
             </h5>
             <a href="">
               Buy Now
             </a>
           </div>
-        </div>
-        <div class="box">
-          <div class="name">
-            <h6>
-              Diamond Ring
-            </h6>
-          </div>
-          <div class="img-box">
-            <img src="assets/images/i-2.png" alt="prod 2">
-          </div>
-          <div class="detail-box">
-            <h5>
-              $<span>1000.00</span>
-            </h5>
-            <a href="">
-              Buy Now
-            </a>
-          </div>
-        </div>
-        <div class="box">
-          <div class="name">
-            <h6>
-              Diamond Ring
-            </h6>
-          </div>
-          <div class="img-box">
-            <img src="assets/images/i-3.png" alt="prod 3">
-          </div>
-          <div class="detail-box">
-            <h5>
-              $<span>1000.00</span>
-            </h5>
-            <a href="">
-              Buy Now
-            </a>
-          </div>
-        </div>
+        </div>';
+      }
+      ?>
       </div>
       <div class="d-flex justify-content-center">
-        <a href="" class="price_btn">
+        <a href="gallery.php" class="price_btn">
           See More
         </a>
       </div>
