e30b54a0dcf34d6a95ebdab797ff837381b29226
kandirou0 <simo.kandirou15@gmail.com>
2022-11-14 14:20:58 +0100

    update

--- assets/js/achat.js
+++ assets/js/achat.js
@@ -2,7 +2,7 @@ var boxs=document.querySelectorAll(".box");
 boxs.forEach((box)=>{
     box.addEventListener('click',function(){
         var num=box.getAttribute('num');
-        alert(num);
+        location.replace("./achat.php?id="+num+"");
     })
 })
 
